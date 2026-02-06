d3d05f10364f99f8cb958067e0df0e00bce93583
Leyton Addison-Roach <git@laddisonroach.com>
2026-02-06 15:21:10 +0000

    chore: remove copy to clipboard

--- scripts/generate-secret.sh
+++ scripts/generate-secret.sh
@@ -2,7 +2,4 @@
 
 secret="$(openssl rand -base64 24 | head --bytes 32)"
 
-printf '%s\n' "$secret"
-
-printf '%s' "$secret" > /dev/clipboard
-printf '\nCopied secret to clipboard.\n'
+echo "$secret"
