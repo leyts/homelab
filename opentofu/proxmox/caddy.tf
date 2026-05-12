189e5fb89f9c1445e4d3de52ffb04fca5428d765
Leyton Addison-Roach <git@laddisonroach.com>
2026-05-12 03:27:48 +0100

    refactor(opentofu): inline Caddy LXC config and disable nesting

--- opentofu/proxmox/caddy.tf
+++ opentofu/proxmox/caddy.tf
@@ -1,41 +1,18 @@
-variable "caddy_lxc" {
-  description = "Caddy LXC settings."
-  type = object({
-    hostname       = string
-    vm_id          = number
-    ipv4_address   = string
-    ipv4_gateway   = string
-    swap_mb        = number
-    rootfs_size_gb = number
-  })
-  default = {
-    hostname       = "lxc-proxy-01"
-    vm_id          = 201
-    ipv4_address   = "192.168.0.201/24"
-    ipv4_gateway   = "192.168.0.1"
-    swap_mb        = 512
-    rootfs_size_gb = 8
-  }
-}
-
 resource "proxmox_virtual_environment_container" "caddy" {
-  node_name    = var.proxmox_node_name
-  vm_id        = var.caddy_lxc.vm_id
-  tags         = ["network"]
+  node_name = var.proxmox_node_name
+  vm_id     = 201
+  tags      = ["network"]
 
   # Newer linux distributions require unprivileged user namespaces
   unprivileged = true
-  features {
-    nesting = true
-  }
 
   initialization {
-    hostname = var.caddy_lxc.hostname
+    hostname = "lxc-proxy-01"
 
     ip_config {
       ipv4 {
-        address = var.caddy_lxc.ipv4_address
-        gateway = var.caddy_lxc.ipv4_gateway
+        address = "192.168.0.201/24"
+        gateway = "192.168.0.1"
       }
     }
 
@@ -56,11 +33,11 @@ resource "proxmox_virtual_environment_container" "caddy" {
 
   disk {
     datastore_id = var.rootfs_datastore_id
-    size         = var.caddy_lxc.rootfs_size_gb
+    size         = 8
   }
 
   memory {
-    swap = var.caddy_lxc.swap_mb
+    swap = 512
   }
 
   wait_for_ip {
@@ -71,9 +48,9 @@ resource "proxmox_virtual_environment_container" "caddy" {
 output "caddy_lxc" {
   description = "Connection details for the Caddy LXC."
   value = {
-    hostname     = var.caddy_lxc.hostname
+    hostname     = proxmox_virtual_environment_container.caddy.initialization[0].hostname
     vm_id        = proxmox_virtual_environment_container.caddy.vm_id
-    ipv4_address = var.caddy_lxc.ipv4_address
+    ipv4_address = "192.168.0.201/24"
     ssh_user     = "root"
   }
 }
